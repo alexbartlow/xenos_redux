fecc7800b07681f5a60a35dc82062d168d95a032
Alex Bartlow <bartlowa@gmail.com>
2014-04-12 19:15:29 -0400

    Flagrant style ripped from mars hill, finish the pull, app to view it

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'json'
+require 'haml'
+require 'nokogiri'
+require 'uri'
+
+def fetch_file(path)
+  page = JSON.parse(
+    File.read(
+      File.join("output/www.xenos.org", path)
+  ))
+  
+  noko = Nokogiri::HTML.parse(page['content'])
+
+  noko.search('a').each do |a|
+    link = URI.parse(a['href'] || '').path
+
+    if link && %w{.html .htm}.include?(File.extname(link))
+      a['href'] = link
+    end
+  end
+
+  page['content'] = noko.to_html
+
+  return page
+end
+
+get '/' do
+  @page = fetch_file('index.htm')
+  haml :page, :format => :html5
+end
+
+get '/stylesheet.css' do
+  sass :app
+end
+
+get '/*' do |path|
+  return 404 if path.include?('..')
+  @page = fetch_file(path)
+  haml :page, :format => :html5
+end
+
